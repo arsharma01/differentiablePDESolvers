1a84c2d76b850bb765389128ee88aa8fc3562b1a
arsharma01 <124182678+arsharma01@users.noreply.github.com>
2024-07-12 13:32:11 -0400

    solver_mod_complete

--- Code and Experiments/dirichlet_solver.py
+++ Code and Experiments/dirichlet_solver.py
@@ -61,3 +61,97 @@ def solve(x = 1/4,y = 1/4, z=0, eps = 1e-4,max_iters = 1000):
     # taking respective means
     return running_function_value/max_iters, running_gradient_value/max_iters
 
+
+# loss function for gradient descent
+# return function value and gradient
+def loss_function(x = 1/4,y = 1/4, z=10, target_value = 15, eps = 1e-4,max_iters = 500):
+    function_value, gradient = solve(x = 1/4,y = 1/4, z=z, eps = 1e-4,max_iters = 500)
+    gradient_of_loss_funct = -2*gradient*(target_value - function_value)
+
+    return (function_value - target_value)**2 , gradient_of_loss_funct
+
+# solves for an inverse problem using gradient descent
+def solve_inverse_grad_dsc( x =1/4, y =1/4, z_init = 30, target_value = 15, loss_function, learning_rate, num_iterations,eps = 1e-4,max_iters_for_solve = 500, progress_report = False) :
+    z = z_init
+    for i in range(num_iterations):
+        # Compute the loss function value and gradient w.r.t. z
+        loss_value, gradient = loss_function(x,y, z, target_value, eps,max_iters_for_solve)
+        
+        if loss_value<0.01:
+            return z
+        
+        # Update parameters
+        z = z - learning_rate * gradient
+        
+        # Optionally print progress
+        if progress_report and i%20 == 0:
+            print(f"Iteration {i+1}: loss = {loss_value}, z = {z}")
+    
+    return z
+
+# consider the unit square with temperature z at the x-axis. 
+# sequential search for unit square
+def seq_search(x = 1/4, y =1/4, target_temp = 15, heater_low = 0, heater_high= 100, error_min = 0.1, prog_report = False):
+    errors_low = []
+    errors_high = []
+    count = 0
+    # initial
+    temp_low = solve(x,y,z = heater_low)[0]
+    low_error =np.abs(temp_low - target_temp) 
+    errors_low.append(low_error)
+
+    temp_high = solve(z = heater_high)[0]
+    high_error =np.abs(temp_high - target_temp) 
+    errors_high.append(high_error)
+
+    if target_temp<temp_low or target_temp>temp_high:
+        print(f"Initial heater settings are insufficent")
+        print(f"Initial temperatures are: {temp_low} and {temp_high}")
+        print("Please readjust")
+        return
+    #success
+    if low_error< error_min:
+        return heater_low       
+    if high_error< error_min:
+        return heater_high
+    
+    count+=1
+    
+    # repeat
+    while count <=2 or jnp.abs(errors_low[-1] - errors_low[-2])/errors_low[-1] > error_min or jnp.abs(errors_low[-1] - errors_low[-2])/errors_low[-1] > error_min:
+           
+        # if closer to lower temperature
+        if low_error < high_error:
+            heater_high = (heater_high+heater_low)/2
+            temp_high = solve(z = heater_high)[0]
+            high_error =np.abs(temp_high - target_temp) 
+            errors_high.append(high_error)
+            if high_error< error_min:
+                return heater_high
+        # if closer to higher temperature
+        else:
+            heater_low = (heater_high+heater_low)/2
+            temp_low = solve(z = heater_low)[0]
+            low_error =np.abs(temp_low - target_temp) 
+            errors_low.append(low_error)
+            if low_error< error_min:
+                return heater_low
+            
+        if prog_report:
+            count += 1
+            if count%10 == 0:
+                print(f"Iter: {count}, error_low: {low_error}, error_low:{high_error}")
+                print(f"temp_low: {heater_low}, temp_high:{heater_high}")
+
+            print(f"Iter: {count}, error_low: {low_error}, error_low:{high_error}")
+            print(f"temp_low: {temp_low}, temp_high:{temp_high}")
+            print(f"heater_low: {heater_low}, heater_high:{heater_high}")
+
+
+    if prog_report:
+        print("we reach final step")
+    if errors_low[-1] < errors_high[-1]:
+        return heater_low
+    else:
+        return heater_high
+        
